3dd61f3516be32cfed6c11042282e5086a23b7a0
J <J@a69356fa-e397-fc40-b0b1-8ef1cfce23d0>
2013-08-26 08:49:30 +0000

    git-svn-id: https://codiato.varaq.com:8443/svn/Codiato.Home/trunk@26 a69356fa-e397-fc40-b0b1-8ef1cfce23d0

---
+++ Codiato.Home/Codiato.Home.Tests/Controllers/BlogPanelControllerTests/CreatePostTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Codiato.Home.WebUI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codiato.Home.Tests.Controllers.BlogPanelControllerTests
+{
+    [TestClass]
+    public class CreatePostTests
+    {
+        [TestMethod]
+        public void Returns_Correct_View_CreatePost_BlogPanel()
+        {
+            // Arrange
+            BlogPanelController cut = new BlogPanelController();
+            
+            // Act
+            ViewResult result = cut.CreatePost() as ViewResult;
+            
+            // Assert
+            Assert.AreEqual(result.ViewName, "Poster");
+        }
+
+    }
+}
