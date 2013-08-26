81a37fd986053021cd5a1120fe220cbc9cba1366
miladkdz <miladkdz@a69356fa-e397-fc40-b0b1-8ef1cfce23d0>
2013-08-26 05:47:13 +0000

    git-svn-id: https://codiato.varaq.com:8443/svn/Codiato.Home/trunk@20 a69356fa-e397-fc40-b0b1-8ef1cfce23d0

---
+++ Codiato.Home/Codiato.Home.Tests/Controllers/BlogController/IndexTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codiato.Home.Tests.Controllers.BlogController
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void Index_Viewbag_Values_Are_Correct()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Inconclusive();
+        }
+			
+    }
+}
